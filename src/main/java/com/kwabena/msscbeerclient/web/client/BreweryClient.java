4fd9e286337c0fd947c7c0396463fdf647c3e12d
kbbansi <kwabenaampofo5@gmail.com>
2023-01-09 10:24:09 +0000

    externalize properties

--- src/main/java/com/kwabena/msscbeerclient/web/client/BreweryClient.java
+++ src/main/java/com/kwabena/msscbeerclient/web/client/BreweryClient.java
@@ -2,6 +2,7 @@ package com.kwabena.msscbeerclient.web.client;
 
 import com.kwabena.msscbeerclient.web.model.BeerDto;
 import com.kwabena.msscbeerclient.web.model.CustomerDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -11,11 +12,11 @@ import java.net.URI;
 import java.util.UUID;
 
 @Component
-@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
 public class BreweryClient {
 
     public final String BEER_PATH_V1 = "/api/v1/beer/";
     public final String CUSTOMER_PATH_V1 = "/api/v1/customer/";
+    @Value("${sfg.brewery.apihost}")
     private String apihost;
     private final RestTemplate restTemplate;
 
@@ -23,9 +24,9 @@ public class BreweryClient {
         this.restTemplate = templateBuilder.build();
     }
 
-    public void setApiHost(String apiHost) {
-        this.apihost = apiHost;
-    }
+//    public void setApiHost(String apiHost) {
+//        this.apihost = apiHost;
+//    }
 
     public BeerDto getBeerById(UUID uuid) {
         return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
